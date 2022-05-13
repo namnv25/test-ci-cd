a3875dd49c710e12754a3fdf26cfbdc60b70ad10
namnv25 <nam.nv@hesperiafreepress.com>
2022-05-13 17:18:38 +0700

    update code

--- nextjs/pages/about.tsx
+++ nextjs/pages/about.tsx
@@ -5,7 +5,7 @@ function About() {
   return (
     <>
       <Link href="/">
-        <a>Home test docker pull image</a>
+        <a>Home test docker pull image tesst -123</a>
       </Link>
       <h1>About test 123</h1>
       <img src="/static/about.jpg" />
